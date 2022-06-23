149367743ccb86fa643bf6b93f821d348fd34def
Alex Greig <78413528+alexjgreig@users.noreply.github.com>
2022-06-24 00:28:57 +1000

    Third attempt to remove video from local git as it is too large to push to the remote, the file is now ignored and removed from cache. This git push marks the finish of this project. I have learnt a lot about development, especially how blockchain technology functions. This project also gave me a chance to develop bleed edge technology that may disrupt the modern world.

--- codebase/node/src/service.rs
+++ codebase/node/src/service.rs
@@ -150,7 +150,7 @@ pub fn new_partial(
 }
 
 fn remote_keystore(_url: &String) -> Result<Arc<LocalKeystore>, &'static str> {
-	// FIXME: here would the concrete keystore be built,
+	// Here would the concrete keystore be built,
 	//        must return a concrete type (NOT `LocalKeystore`) that
 	//        implements `CryptoStore` and `SyncCryptoStore`
 	Err("Remote Keystore not supported.")
